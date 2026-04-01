efee507214527f5f18770f9124f911d64efc409c
kxthzr <kxthzr@gmail.com>
2026-04-01 03:54:14 -0500

    q1b

--- sudoku1.py
+++ sudoku1.py
@@ -1,28 +1,30 @@
 import clingo
 import sys
 
+def load_files(ctl, files):
+    for f in files:
+        ctl.load(f)
+
+def print_model(model):
+    atoms = list(model.symbols(shown=True))
+    atoms.sort()
+    print("Answer:", model.number)
+    print(" ".join(map(str, atoms)))
+
 class SolverApp(clingo.Application):
     def __init__(self):
         super().__init__()
         self.program_name = "sudoku"
 
-    def main(self, ctl, input_files):
+    def main(self, ctl, files):
         ctl.load("sudoku.lp")
-
-        for file_name in input_files:
-            ctl.load(file_name)
-
+        load_files(ctl, files)
         ctl.ground([("base", [])])
+        ctl.solve(on_model=print_model)
 
-        result = ctl.solve()
-
-        return result
-
-
-def run():
+def solve(encoding, inputs):
     app = SolverApp()
-    clingo.clingo_main(app, sys.argv[1:])
-
+    clingo.clingo_main(app, inputs)
 
 if __name__ == "__main__":
-    run()
+    solve("sudoku.lp", sys.argv[1:])
